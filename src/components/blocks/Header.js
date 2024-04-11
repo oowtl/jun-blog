371efedada073e1f1a1e1dc2b171bc427d7511b8
Jun <oowtl7425@gmail.com>
2024-04-12 06:36:06 +0900

    feat : header 컴포넌트 - headerNavLinks : 네비게이션을 위한 데이터 - header 컴포넌트

---
+++ src/components/blocks/Header.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+// DATA
+import headerNavLinks from '@/data/headerNavLinks'
+
+// COMPONENTS
+import Logo from '@/components/atoms/Icon/logo.svg'
+import NavigationButtton from '@/components/atoms/NavigationButton/NavigationButtton.jsx'
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="flex justify-between">
+        <h1 className="w-12 h-12">
+          <Link href="/">
+            <Logo className="w-full h-full" />
+          </Link>
+        </h1>
+        <ul className="flex gap-5">
+          {headerNavLinks
+            .filter((navLink) => navLink.href !== '/')
+            .map((navLink) => {
+              return (
+                <li key={navLink.title} className="w-12 h-12 relative">
+                  <NavigationButtton
+                    href={navLink.href}
+                    title={navLink.title}
+                  />
+                </li>
+              )
+            })}
+        </ul>
+      </nav>
+    </header>
+  )
+}
