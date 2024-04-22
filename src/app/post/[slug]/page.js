86b3114fe17996bb222190fb26d1919ac0402f9a
Jun <oowtl7425@gmail.com>
2024-04-22 16:33:22 +0900

    feat : 포스트 보여주기 기능

--- src/app/post/[slug]/page.js
+++ src/app/post/[slug]/page.js
@@ -1,3 +1,24 @@
-export default function PostPage() {
-  return <div>page</div>
+import { getAllPosts } from '@/utills/post'
+
+import CustomMdx from '@/components/blocks/CustomMdx.jsx'
+
+export default async function PostPage({ params }) {
+  const slug = decodeURI(params.slug)
+  const postList = getAllPosts()
+
+  const curPostIndex = postList.findIndex((post) => post.title === slug)
+  // TODO : post를 찾을 수 없을 때 에러처리
+  if (curPostIndex === -1) {
+    console.log('no cotent')
+  }
+
+  const curPost = postList[curPostIndex]
+  const nextPost = postList[curPostIndex + 1]
+  const prevPost = postList[curPostIndex - 1]
+
+  return (
+    <article className="prose prose-lg prose-zinc m-auto">
+      <CustomMdx source={curPost.content} />
+    </article>
+  )
 }
